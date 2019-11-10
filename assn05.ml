66b9e3664763b9814fe821b2c7f23654bb6f4b8a
Rahul Mitra (rmitra) <rmitra@syslab10.cs.trincoll.edu>
2019-11-09 19:05:28 -0500

    added dot product

--- assn05.ml
+++ assn05.ml
@@ -1,15 +1,27 @@
 (* definition of cycle() *)
 fun cycle (L, n) 
-= if n = 0 then L
+= if L = nil then L
+else if n = 0 then L
 else cycle(tl L @ [hd L], n -1);
 
 (* testing cycle() *)
+cycle([],1);
 cycle([1,2,3,4],1);
 cycle([1,2,3,4],3);
 
 (*definition of dup()*)
-fun dup nil = nil
+fun dup [] = []
   | dup (x::xs) = x :: x :: dup xs;
   
 (*testing of dup()*)
 dup([1,2,3]);
+dup([1,2,3,4]);
+dup([1,2]);
+
+(*definition of dot*)
+fun dot(L1, L2) = 
+if null L1 then 0.0
+else (hd L1 * hd L2) + dot(tl L1,tl L2);
+
+(*testing of dot*)
+dot([1.0,2.0,3.0],[1.0,1.0,1.0]);
